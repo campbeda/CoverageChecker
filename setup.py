04e547ab52971081898f83d6ff0fb5a8a5d06468
Colton Saska <colt.saska@gmail.com>
2020-04-20 21:51:46 -0500

    Add framework for PyPi packaging. Conform to PEP8 package/module naming convention.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diffcoverage", # Replace with your own username
+    version="0.0.1",
+    author="Jeff Nelson, Dave Campbell, and Colton Saska",
+    author_email="author@example.com",
+    description="Set of tools for validating code coverage as a merge criterion in a CI pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdn5126/CoverageChecker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
